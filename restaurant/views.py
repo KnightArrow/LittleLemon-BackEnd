145842b7fda84965c2c2cd54bb4a0b013fe57a30
KnightArrow <tiwarivibhu193@gmail.com>
2024-06-23 20:53:48 +0530

    After Djoser

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
-
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
@@ -15,6 +15,7 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes=[IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
